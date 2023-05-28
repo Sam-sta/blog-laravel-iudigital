46eabe369f3635780e45f847224502569bb9e8d4
Sam-sta <sasanchez3010@gmail.com>
2023-05-28 17:20:06 -0500

    Created migration for posts

--- database/migrations/2023_05_28_220612_create_posts_table.php
+++ database/migrations/2023_05_28_220612_create_posts_table.php
@@ -11,11 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 500)->nullable();
             $table->string('slug', 500)->nullable();
+            $table->text('description')->nullable();
+            $table->text('content')->nullable();
+            $table->string('image')->nullable();
+            $table->enum('posted', ['yes', 'not'])->nullable()->default('not');
             $table->timestamps();
+
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('cascade');
         });
     }
 
@@ -24,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 };
