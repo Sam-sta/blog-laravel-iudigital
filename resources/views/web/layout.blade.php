b820bd6d10530d5013d80a09a9a46025cc8e3ebe
Sam-sta <sasanchez3010@gmail.com>
2023-05-28 16:15:22 -0500

    Added laravel/ui for atuhentication

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -13,13 +13,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Pagina de inicio</h2>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container">
+        <div class="card card-white mt-4">
+            <div class="card-body">
+                @yield('content')
+            </div>
         </div>
     </div>
 </body>
