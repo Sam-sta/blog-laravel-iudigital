e1054ca2044f4ad4a55b4d8359e731b661e9b292
Sam-sta <sasanchez3010@gmail.com>
2023-06-01 08:36:19 -0500

    Removed form tag from blade

--- resources/views/dashboard/categories/_form.blade.php
+++ resources/views/dashboard/categories/_form.blade.php
@@ -1,17 +1,14 @@
-<form action="{{ route('categories.store') }}" method="POST">
-    @csrf
+@csrf
 
-    <div class="form-group">
-        <label for="title">Titulo</label>
-        <input class="form-control" type="text" name="title" id="title"
-            value="{{ old('title', $category->title) }}">
-    </div>
+<div class="form-group">
+    <label for="title">Titulo</label>
+    <input class="form-control" type="text" name="title" id="title"
+        value="{{ old('title', $category->title) }}">
+</div>
 
-    <div class="form-group">
-        <label for="slug">Slug</label>
-        <input class="form-control" type="text" name="slug" id="slug"
-            value="{{ old('slug', $category->slug) }}">
-    </div>
-    <input class="btn btn-primary" type="submit" value="Guardar">
-
-</form>
+<div class="form-group">
+    <label for="slug">Slug</label>
+    <input class="form-control" type="text" name="slug" id="slug"
+        value="{{ old('slug', $category->slug) }}">
+</div>
+<input class="btn btn-primary" type="submit" value="Guardar">
