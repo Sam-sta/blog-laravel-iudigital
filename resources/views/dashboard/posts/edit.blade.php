2d9bc62305c8344e52513fc0e3a5238db02fe19b
Sam-sta <sasanchez3010@gmail.com>
2023-06-01 08:29:06 -0500

    Created edit blade for post

---
+++ resources/views/dashboard/posts/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="content-wrapper">
+        <h1>Actualizar categoría {{ $post->title }}</h1>
+        @include('dashboard.fragment._errors-form')
+        <form action="{{ route('posts.update', $post->id) }}" method="post">
+            @method('PATCH')
+            @include('dashboard.posts._form')
+        </form>
+    </div>
+@endsection
